bb637535e0dd49678568a982c479dd665f180187
Jereme Causing <jeremejazz15@yahoo.com>
2016-12-10 12:43:18 +0000

    update build script

--- build.sh
+++ build.sh
@@ -3,12 +3,10 @@
 
 #remove public content 
 echo "removing public "
-git rm -r public  
+  
 
 rm -rf public
-git subtree add --prefix=public git@github.com:jeremejazz/jeremejazz.github.com.git master--squash
-
-git subtree pull --prefix=public  git@github.com:jeremejazz/jeremejazz.github.com.git master
+mkdir public 
 #run hugo compiler
 echo "building public folder"
 ../bin/hugo
@@ -21,15 +19,3 @@ lessc less/freelancer.less public/css/freelancer.css
 #TODO minify. but might require to configure theme repo. 
 # bette off this way for now
 
-
-echo "Starting deployment... press enter to Continue. CTRL + C to cancel"
-read -p "Press enter to continue"
-
-
-function deploy(){
-
-}
-
-deploy
-
-#enter credentials here  
