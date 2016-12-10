bc4f4d2775ea9e7468542d95a470ab3acf73c5ce
Jereme Causing <jeremejazz15@yahoo.com>
2016-12-10 11:12:37 +0000

    update build file

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
 #! usr/local/bin
+
+
+#remove public content 
+git rm -r public  
+
+
+#run hugo compiler
 ../bin/hugo
+
+echo "Deploying..."
+cd public 
+git add .
+git commit -m "update website"
+echo "Committed repo"
+echo "Pushing to origin, please enter credentials"
+git push origin master
+
+#enter credentials here  
